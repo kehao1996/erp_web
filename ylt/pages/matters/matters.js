943efd25d454d383a0fec44891b62561fe0c376f
周舒 <624350333@qq.com>
2020-07-20 18:52:22 +0800

    0000

--- ylt/pages/matters/matters.js
+++ ylt/pages/matters/matters.js
@@ -5,7 +5,101 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    mattersData:[
+      {
+        img:"../../images/mat3.png",
+        name:"基本薪资",
+        inptname:"请输入数值大小",
+        btnoff:1
+      },
+      {
+        img: "../../images/mat4.png",
+        name: "绩效基础",
+        inptname: "请输入数值大小",
+        btnoff: 1
+      },
+      {
+        img: "../../images/mat5.png",
+        name: "任务计划时效",
+        inptname: "请输入数值",
+        btnoff: 1
+      },
+      {
+        img: "../../images/mat6.png",
+        name: "实际完成时效",
+        inptname: "99999.00",
+        btnoff: 1
+      },
+      {
+        img: "../../images/mat7.png",
+        name: "奖惩比例",
+        inptname: "99999.00",
+        btnoff: 1
+      },
+      {
+        img: "../../images/mat8.png",
+        name: "完成时效绩效",
+        inptname: "99999.00",
+        btnoff: 0
+      },
+      {
+        img: "../../images/mat9.png",
+        name: "质检达成率",
+        inptname: "99999.00",
+        btnoff: 1
+      },
+      {
+        img: "../../images/mat10.png",
+        name: "质量绩效",
+        inptname: "99999.00",
+        btnoff: 1
+      },
+      {
+        img: "../../images/mat10.png",
+        name: "质量绩效",
+        inptname: "99999.00",
+        btnoff: 0
+      },
+      {
+        img: "../../images/mat11.png",
+        name: "考勤绩效",
+        inptname: "99999.00",
+        btnoff: 1
+      },
+      {
+        img: "../../images/mat12.png",
+        name: "总计",
+        inptname: "99999.00",
+        btnoff: 3
+      }
+    ],
+    painterData:[
+      {
+        img: "../../images/mat3.png",
+        name: "基本薪资",
+        inptname: "请输入数值大小"
+      },
+      {
+        img: "../../images/mat14.png",
+        name: "记件议定价",
+        inptname: "请输入数值大小"
+      },
+      {
+        img: "../../images/mat15.png",
+        name: "绩效考核",
+        inptname: "请输入正负数"
+      },
+      {
+        img: "../../images/mat16.png",
+        name: "数量",
+        inptname: "99999.00"
+      },
+      {
+        img: "../../images/mat12.png",
+        name: "总计",
+        inptname: "99999.00"
+      }
+    ]
   },
 
   /**
